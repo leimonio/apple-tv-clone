9832bdbb8a569a5e7d470d1e86228e0d6b315df9
Konstantinos Leimonis <k.leimonis@gmail.com>
2023-03-25 08:08:01 +0000

    chore: remove most videos from the repo and db

--- src/db.js
+++ src/db.js
@@ -1,13 +1,4 @@
-import Defending_Jacob from './assets/videos/Defending_Jacob_.mp4'
-import Finch from './assets/videos/Finch.mp4'
-import Fondation from './assets/videos/fondation.mp4'
-import Mankind from './assets/videos/for all mankind.mp4'
 import Home from './assets/videos/home.mp4'
-import Morning from './assets/videos/morning.mp4'
-import See from './assets/videos/see.mp4'
-import Sev from './assets/videos/sev.mp4'
-import Tehran from './assets/videos/Tehran.mp4'
-import Suspicion from './assets/videos/Suspicion.mp4'
 
 export const cards = [
   {
@@ -52,21 +43,21 @@ const comingSoon = [
     image:
       'https://is1-ssl.mzstatic.com/image/thumb/Features116/v4/d8/b8/bf/d8b8bff6-e7b0-5be4-9930-a779aea319e3/U0YtVFZBLVdXLVRoZV9Qcm9ibGVtX0pvbl9TdGV3YXJ0Lmxzcg.lsr/1478x832fe.webp',
     info: 'New Episodes Premiere March 3',
-    video: Defending_Jacob,
+    video: Home,
   },
   {
     id: 2,
     image:
       'https://is1-ssl.mzstatic.com/image/thumb/Features126/v4/93/50/f4/9350f4f6-4c50-f6a5-6976-446a5333b265/U0ZfVFZBX1dXX0RlYXItUzItQ29taW5nX1Nvb24tQnJpY2subHNy.lsr/1478x832fe.webp',
     info: 'New Season Premiere March 3',
-    video: Fondation,
+    video: Home,
   },
   {
     id: 3,
     image:
       'https://is2-ssl.mzstatic.com/image/thumb/Features125/v4/db/34/3b/db343b95-7cd0-2001-0282-771fff181727/U0YtVFZBLVdXLUNlbnRyYWxfUGFyay1TMkEtTGF1bmNoLmxzcg.lsr/1478x832fe.webp',
     info: 'New Episodes Premiere March 4',
-    video: Finch,
+    video: Home,
   },
   {
     id: 4,
@@ -80,42 +71,42 @@ const comingSoon = [
     image:
       'https://is2-ssl.mzstatic.com/image/thumb/Features116/v4/96/0d/8e/960d8ef8-5773-9678-7738-c74c9d763853/U0YtVFZBLVdXLVRoZV9Tbm9vcHlfU2hvdy1TMkEtQ1NfQnJpY2subHNy.lsr/1478x832fe.webp',
     info: 'New Season Premiere March 11',
-    video: Mankind,
+    video: Home,
   },
   {
     id: 6,
     image:
       'https://is2-ssl.mzstatic.com/image/thumb/Features116/v4/32/1e/39/321e395c-2b34-6f5a-3768-764252769471/U0YtVFZBLVdXLVdlY3Jhc2hlZC1DUy1Ccmlja19ub0J1Zy5sc3I.lsr/1478x832fe.webp',
     info: 'New Series Premiere March 18',
-    video: Morning,
+    video: Home,
   },
   {
     id: 7,
     image:
       'https://is4-ssl.mzstatic.com/image/thumb/Features126/v4/e0/04/56/e0045660-db0b-37da-bde5-91a431d28574/U0YtVFZBLVdXLVBhY2hpbmtvLUVhcmx5X0NTLmxzcg.lsr/1478x832fe.webp',
     info: 'New Series Premiere March 25',
-    video: See,
+    video: Home,
   },
   {
     id: 8,
     image:
       'https://is2-ssl.mzstatic.com/image/thumb/Features116/v4/a2/a4/0d/a2a40dbb-06be-1791-d241-019657d17469/U0YtVFZBLVdXLVNsb3dfSG9yc2VzLUNTLUJyaWNrLW5vQnVnLmxzcg.lsr/1478x832fe.webp',
     info: 'New Series Premiere April 1',
-    video: Sev,
+    video: Home,
   },
   {
     id: 9,
     image:
       'https://is3-ssl.mzstatic.com/image/thumb/Features116/v4/25/ee/d1/25eed172-b1de-32c4-ea17-be19f53cf21e/U0YtVFZBLVdXLVNoaW5pbmdfR2lybHMtQ1MubHNy.lsr/1478x832fe.webp',
     info: 'New Series Premiere April 29',
-    video: Suspicion,
+    video: Home,
   },
   {
     id: 10,
     image:
       'https://is1-ssl.mzstatic.com/image/thumb/Features116/v4/7e/e4/05/7ee40559-a590-1c90-9057-d9e14c76af6d/U0YtVFZBLVdXLUx1Y2stQ1MtQnJpY2stbm9CdWcubHNy.lsr/1478x832fe.webp',
     info: 'New Film Premiere August 5',
-    video: Tehran,
+    video: Home,
   },
 ]
 
@@ -124,13 +115,13 @@ const tales = [
     id: 1,
     image:
       'https://is2-ssl.mzstatic.com/image/thumb/6tJakyTEtS0vrgdc9POZpg/738x416.webp',
-    video: Sev,
+    video: Home,
   },
   {
     id: 2,
     image:
       'https://is1-ssl.mzstatic.com/image/thumb/yXhlXOFkS9yJ1kMmOhfIbg/738x416.webp',
-    video: See,
+    video: Home,
   },
   {
     id: 3,
@@ -142,13 +133,13 @@ const tales = [
     id: 4,
     image:
       'https://is5-ssl.mzstatic.com/image/thumb/Cof8vC9vaCH-GYX6JltrQA/738x416.webp',
-    video: Mankind,
+    video: Home,
   },
   {
     id: 5,
     image:
       'https://is4-ssl.mzstatic.com/image/thumb/EjmCDwq97jEEe9RuKY7aNw/738x416.webp',
-    video: Fondation,
+    video: Home,
   },
   {
     id: 6,
@@ -159,7 +150,7 @@ const tales = [
     id: 7,
     image:
       'https://is5-ssl.mzstatic.com/image/thumb/NHLksWLSZpTcIw36-n7vWA/738x416.webp',
-    video: Defending_Jacob,
+    video: Home,
   },
 ]
 
@@ -206,7 +197,7 @@ const voice = [
   {
     image:
       'https://is3-ssl.mzstatic.com/image/thumb/3iAbZQ4pznOpDRenkvOhNA/738x416.webp',
-    vide: Fondation,
+    video: Home,
   },
   {
     image:
