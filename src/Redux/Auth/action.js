b0cda27eb90c3fb63e34f912c64f015bcc782379
Konstantinos Leimonis <k.leimonis@gmail.com>
2023-05-24 17:55:03 +0100

    rename: move redux folder to temp

--- src/Redux/Auth/action.js
+++
@@ -1,9 +0,0 @@
-export const ADD_TOKEN = 'ADD_TOKEN'
-
-// to avoid spelling mistake we store it in the variable.
-export const adduser = (token) => {
-  return {
-    type: ADD_TOKEN,
-    payload: token,
-  }
-}
